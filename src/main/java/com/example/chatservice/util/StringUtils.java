941986cabe8e66be0eb5fb9d24e304356698bac3
Amit Sharma <amiter.cs@gmail.com>
2024-08-11 13:24:21 +0530

    Formatted code

--- src/main/java/com/example/chatservice/util/StringUtils.java
+++ src/main/java/com/example/chatservice/util/StringUtils.java
@@ -2,7 +2,12 @@ package com.example.chatservice.util;
 
 public class StringUtils {
 
+    private StringUtils()
+    {
+
+    }
+
     public static boolean isNullOrEmpty(String data) {
-        return (null == data || "".equals(data.trim())) ? true : false;
+        return (null == data || "".equals(data.trim()));
     }
 }
